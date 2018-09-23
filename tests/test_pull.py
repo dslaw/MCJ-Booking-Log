0b9b2f3e5603ebcb02afd462c6db87465dc7a166
David Law <davidsamuellaw@gmail.com>
2018-09-23 10:08:35 -0700

    Add unit tests for table extraction

--- tests/test_pull.py
+++ tests/test_pull.py
@@ -1,3 +1,4 @@
+from bs4 import BeautifulSoup
 import pytest
 
 from bookinglog import pull
@@ -9,6 +10,81 @@ def html():
         mock_html = fh.read()
     return mock_html
 
+@pytest.fixture
+def table():
+    html = (
+        '<tbody>\n'
+        '  <tr>\n'
+        '    <th scope="col"> First </th>\n'
+        '    <th scope="col"> Second </th>\n'
+        '  </tr>\n'
+        '  <tr>\n'
+        '    <td>11</td>\n'
+        '    <td>12</td>\n'
+        '  </tr>\n'
+        '  <tr>\n'
+        '    <td>21</td>\n'
+        '    <td>22</td>\n'
+        '  </tr>\n'
+        '</tbody>'
+    )
+    return BeautifulSoup(html, "html.parser")
+
+@pytest.fixture
+def keyvalue_table():
+    html = (
+        '<tbody>\n'
+        '  <tr>\n'
+        '    <th scope="row">First:</th>\n'
+        '    <td>Hello</td>\n'
+        '  </tr>\n'
+        '  <tr>\n'
+        '    <th scope="row">Second:</th>\n'
+        '    <td>World</td>\n'
+        '  </tr>\n'
+        '</tbody>'
+    )
+    return BeautifulSoup(html, "html.parser")
+
+
+class TestTable(object):
+    def test_parse_columns(self, table):
+        expected = ["First", "Second"]
+
+        out = pull.parse_columns(table)
+        assert out == expected
+
+    def test_parse_charges(self, table):
+        expected = [{
+            "First": "11",
+            "Second": "12",
+        }, {
+            "First": "21",
+            "Second": "22",
+        }]
+
+        out = pull.parse_charges(table)
+        assert out == expected
+
+class TestKeyValueTable(object):
+    def test_parse_row(self):
+        html = (
+            '<tr>\n'
+            '  <th scope="row">Key:</th>\n'
+            '  <td>Value</td>\n'
+            '</tr>'
+        )
+        row = BeautifulSoup(html, "html.parser")
+        expected = {"Key": "Value"}
+
+        out = pull.parse_row(row)
+        assert out == expected
+
+    def test_parse_table(self, keyvalue_table):
+        expected = {"First": "Hello", "Second": "World"}
+
+        out = pull.parse_table(keyvalue_table)
+        assert out == expected
 
 class TestParse(object):
     def test_mock_data(self, html):
